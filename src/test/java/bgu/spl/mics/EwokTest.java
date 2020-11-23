968531090b5ede9b58682f54aaa811c1f666fbaa
Ofir <ofirkap@gmail.com>
2020-11-23 19:20:20 +0200

    Created EwokTest and built its frame

---
+++ src/test/java/bgu/spl/mics/EwokTest.java
@@ -0,0 +1,28 @@
+package bgu.spl.mics;
+
+import bgu.spl.mics.application.passiveObjects.Ewok;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EwokTest {
+
+    private Ewok ewok;
+
+    @BeforeEach
+    public void setUp(){
+        ewok = new Ewok();
+    }
+
+    @Test
+    public void testAcquire()
+    {
+    }
+
+    @Test
+    public void testRelease()
+    {
+    }
+}
