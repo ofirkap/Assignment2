e9da559fbc9ab2ebb6ec6373f378c8bc2b074f7d
Ofir <ofirkap@gmail.com>
2020-11-24 05:01:00 +0200

    Downloaded the new work files

--- src/main/java/bgu/spl/mics/Future.java
+++ src/main/java/bgu/spl/mics/Future.java
@@ -29,7 +29,7 @@ public class Future<T> {
      * @return return the result of type T if it is available, if not wait until it is available.
      * 	       
      */
-	public synchronized T get() {
+	public T get() {
 		
         return null; 
 	}
@@ -37,14 +37,16 @@ public class Future<T> {
 	/**
      * Resolves the result of this Future object.
      */
-	public synchronized void resolve (T result) {
+	public void resolve (T result) {
 		
 	}
 	
 	/**
      * @return true if this object has been resolved, false otherwise
      */
-	public synchronized boolean isDone() { return null; }
+	public boolean isDone() {
+		return null;
+	}
 	
 	/**
      * retrieves the result the Future object holds if it has been resolved,
@@ -57,8 +59,9 @@ public class Future<T> {
      * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
      *         elapsed, return null.
      */
-	public synchronized T get(long timeout, TimeUnit unit) {
+	public T get(long timeout, TimeUnit unit) {
 		
         return null;
 	}
+
 }
--- src/test/java/bgu/spl/mics/EwokTest.java
+++ src/test/java/bgu/spl/mics/EwokTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+//need to write code
 public class EwokTest {
 
     private Ewok ewok;
